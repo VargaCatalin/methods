d48a0ccabb876f792ca9f6690015478315323ed1
VargaCatalin <catavarga@yahoo.com>
2020-03-26 19:21:14 +0200

    Created  math operations

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,6 +8,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        double a = 20, b = 10;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        Operations operations = new Operations();
+
+        double result = operations.add(a, b);
+        System.out.println("a + b = " + result);
+
+        result = operations.subtract(a, b);
+        System.out.println("a - b = " + result);
+
+        result = operations.multiply(a, b);
+        System.out.println("a * b = " + result);
+
+        result = operations.divide(a, b);
+        System.out.println("a / b = " + result);
     }
+
+
+    
 }
