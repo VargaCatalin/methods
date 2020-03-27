8e62d3ebbe4ba9f686a2140974f814899c204f71
VargaCatalin <catavarga@yahoo.com>
2020-03-27 21:15:26 +0300

    Created overloading methods

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -2,14 +2,12 @@ package org.fasttrackit;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
 
-        double a = 20, b = 10;
+        double a = 20.2;
+        double b = 10.5;
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
@@ -27,8 +25,27 @@ public class App
 
         result = operations.divide(a, b);
         System.out.println("a / b = " + result);
+
+
+    int c = 20 , d = 10;
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+
+        int result2 = operations.add(c, d);
+        System.out.println("c + d = " + result);
+
+        result = operations.subtract(c, d);
+        System.out.println("c - d = " + result);
+
+        result = operations.multiply(c, d);
+        System.out.println("c * d = " + result);
+
+        result = operations.divide(c, d);
+        System.out.println("c / d = " + result);
+
+
+
+
     }
 
-
-    
 }
--- src/main/java/org/fasttrackit/Operations.java
+++ src/main/java/org/fasttrackit/Operations.java
@@ -6,16 +6,38 @@ public class Operations {
         return x + y;
     }
 
+    public int add(int x, int y) {
+        return x + y;
+
+    }
+
     public double subtract(double x, double y){
         return x - y;
     }
 
+    public int subtract(int  x, int y){
+        return x - y;
+    }
+
+
+
     public double multiply(double x, double y){
+        return x * y;
+    }
+
+    public int multiply(int x, int y) {
         return x * y;
     }
 
     public double divide(double x, double y){
         return x / y;
     }
+
+    public int  divide(int  x, int y){
+        return x / y;
+    }
+
+
+
 
 }
